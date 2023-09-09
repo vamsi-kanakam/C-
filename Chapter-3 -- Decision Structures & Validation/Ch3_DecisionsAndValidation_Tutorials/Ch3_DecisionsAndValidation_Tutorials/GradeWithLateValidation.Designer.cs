5086b7f58e6d4836d30969f3ea703da5e50f099b
vamsi-kanakm <vamsikanakam2002@gmail.com>
2023-09-08 22:52:28 -0400

    Tutorial-4 Late Validation

---
+++ Chapter-3 -- Decision Structures & Validation/Ch3_DecisionsAndValidation_Tutorials/Ch3_DecisionsAndValidation_Tutorials/GradeWithLateValidation.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Ch3_DecisionsAndValidation_Tutorials
+{
+    partial class GradeWithLateValidation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnComputeGrade = new System.Windows.Forms.Button();
+            this.txtGrade = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAttendance = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTotalScore = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnComputeGrade
+            // 
+            this.btnComputeGrade.Location = new System.Drawing.Point(353, 193);
+            this.btnComputeGrade.Name = "btnComputeGrade";
+            this.btnComputeGrade.Size = new System.Drawing.Size(126, 39);
+            this.btnComputeGrade.TabIndex = 13;
+            this.btnComputeGrade.Text = "Compute";
+            this.btnComputeGrade.UseVisualStyleBackColor = true;
+            this.btnComputeGrade.Click += new System.EventHandler(this.btnComputeGrade_Click);
+            // 
+            // txtGrade
+            // 
+            this.txtGrade.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.txtGrade.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.txtGrade.Location = new System.Drawing.Point(376, 322);
+            this.txtGrade.Name = "txtGrade";
+            this.txtGrade.Size = new System.Drawing.Size(85, 33);
+            this.txtGrade.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(273, 322);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 25);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Grade:";
+            // 
+            // txtAttendance
+            // 
+            this.txtAttendance.Location = new System.Drawing.Point(612, 96);
+            this.txtAttendance.Name = "txtAttendance";
+            this.txtAttendance.Size = new System.Drawing.Size(100, 31);
+            this.txtAttendance.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(453, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 25);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Attendance:";
+            // 
+            // txtTotalScore
+            // 
+            this.txtTotalScore.Location = new System.Drawing.Point(239, 96);
+            this.txtTotalScore.Name = "txtTotalScore";
+            this.txtTotalScore.Size = new System.Drawing.Size(100, 31);
+            this.txtTotalScore.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(88, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 25);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Total Score:";
+            // 
+            // GradeWithLateValidation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnComputeGrade);
+            this.Controls.Add(this.txtGrade);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtAttendance);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTotalScore);
+            this.Controls.Add(this.label1);
+            this.Name = "GradeWithLateValidation";
+            this.Text = "GradeWithLateValidation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnComputeGrade;
+        private System.Windows.Forms.Label txtGrade;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAttendance;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTotalScore;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Chapter-3 -- Decision Structures & Validation/Ch3_DecisionsAndValidation_Tutorials/Ch3_DecisionsAndValidation_Tutorials/GradeWithLateValidation.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ch3_DecisionsAndValidation_Tutorials
+{
+    public partial class GradeWithLateValidation : Form
+    {
+        public GradeWithLateValidation()
+        {
+            InitializeComponent();
+        }
+
+        private void btnComputeGrade_Click(object sender, EventArgs e)
+        {
+            string letterGrade;
+            float totalScore, attendance;
+            if(float.TryParse(txtTotalScore.Text,out totalScore) == false)
+            {
+                MessageBox.Show("Plese Enter a valid number for Total Score.");
+                txtTotalScore.Focus();
+                txtTotalScore.SelectAll();
+            }
+            if(float.TryParse(txtAttendance.Text,out attendance) == false)
+            {
+                MessageBox.Show("Plese Enter a valid number for Attendance.");
+                txtAttendance.Focus();
+                txtAttendance.SelectAll();
+            }
+
+            if (totalScore >= 90f)
+            {
+                if (attendance >= 90f)
+                    letterGrade = "A";
+                else
+                    letterGrade = "B";
+            }
+            else if (totalScore >= 80f)
+            {
+                letterGrade = "B";
+            }
+            else if (totalScore >= 70f)
+                letterGrade = "C";
+            else
+                letterGrade = "F";
+
+            txtGrade.Text = null;
+            txtGrade.Text = letterGrade;
+        }
+    }
+}
