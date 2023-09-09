31d8bad7db9b3531b51699b38370507e60467309
vamsi-kanakm <vamsikanakam2002@gmail.com>
2023-09-08 22:22:01 -0400

    Tutorial 3 - Switch Cases

---
+++ Chapter-3 -- Decision Structures & Validation/Ch3_DecisionsAndValidation_Tutorials/Ch3_DecisionsAndValidation_Tutorials/Reservation.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Ch3_DecisionsAndValidation_Tutorials
+{
+    partial class Reservation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDiscountType = new System.Windows.Forms.TextBox();
+            this.btnCompute = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblDiscountPercent = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(198, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Discount Type:";
+            // 
+            // txtDiscountType
+            // 
+            this.txtDiscountType.Location = new System.Drawing.Point(415, 76);
+            this.txtDiscountType.Name = "txtDiscountType";
+            this.txtDiscountType.Size = new System.Drawing.Size(165, 31);
+            this.txtDiscountType.TabIndex = 1;
+            // 
+            // btnCompute
+            // 
+            this.btnCompute.AutoSize = true;
+            this.btnCompute.Location = new System.Drawing.Point(311, 168);
+            this.btnCompute.Name = "btnCompute";
+            this.btnCompute.Size = new System.Drawing.Size(134, 35);
+            this.btnCompute.TabIndex = 2;
+            this.btnCompute.Text = "Compute";
+            this.btnCompute.UseVisualStyleBackColor = true;
+            this.btnCompute.Click += new System.EventHandler(this.btnCompute_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(203, 272);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(182, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Discount Percent:";
+            // 
+            // lblDiscountPercent
+            // 
+            this.lblDiscountPercent.AutoSize = true;
+            this.lblDiscountPercent.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblDiscountPercent.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblDiscountPercent.Location = new System.Drawing.Point(415, 272);
+            this.lblDiscountPercent.Name = "lblDiscountPercent";
+            this.lblDiscountPercent.Size = new System.Drawing.Size(2, 27);
+            this.lblDiscountPercent.TabIndex = 4;
+            // 
+            // Reservation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblDiscountPercent);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnCompute);
+            this.Controls.Add(this.txtDiscountType);
+            this.Controls.Add(this.label1);
+            this.Name = "Reservation";
+            this.Text = "Reservation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDiscountType;
+        private System.Windows.Forms.Button btnCompute;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblDiscountPercent;
+    }
+}
---
+++ Chapter-3 -- Decision Structures & Validation/Ch3_DecisionsAndValidation_Tutorials/Ch3_DecisionsAndValidation_Tutorials/Reservation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ch3_DecisionsAndValidation_Tutorials
+{
+    public partial class Reservation : Form
+    {
+        public Reservation()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCompute_Click(object sender, EventArgs e)
+        {
+            string discountType = txtDiscountType.Text;
+            float discountPercent;
+
+            switch(discountType)
+            {
+                case "AAA":
+                    discountPercent = 10;
+                    break;
+                case "Best Rate":
+                    discountPercent= 5;
+                    break;
+                case "Government":
+                    discountPercent = 15;
+                    break;
+                case "Senior":
+                    discountPercent = 12.5f;
+                    break;
+                default:
+                    discountPercent = 0;
+                    break;
+            }
+
+            lblDiscountPercent.Text = discountPercent.ToString("N");
+        }
+    }
+}
