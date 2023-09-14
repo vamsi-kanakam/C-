e8b7a4449122de9953932706ca1f39a3c287870e
vamsi-kanakm <vamsikanakam2002@gmail.com>
2023-09-14 01:27:24 -0400

    Chapter 4 Loops Added

---
+++ Chapter-4 -- Iteration Structures -- Loops/Ch4_Loops -- Tutorials/Ch4_Loops/VerifyPassword.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Ch4_Loops
+{
+    partial class VerifyPassword
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnContinue = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnContinue
+            // 
+            this.btnContinue.AutoSize = true;
+            this.btnContinue.Location = new System.Drawing.Point(337, 202);
+            this.btnContinue.Name = "btnContinue";
+            this.btnContinue.Size = new System.Drawing.Size(108, 35);
+            this.btnContinue.TabIndex = 0;
+            this.btnContinue.Text = "Continue";
+            this.btnContinue.UseVisualStyleBackColor = true;
+            this.btnContinue.Click += new System.EventHandler(this.btnContinue_Click);
+            // 
+            // VerifyPassword
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnContinue);
+            this.Name = "VerifyPassword";
+            this.Text = "Verify Password";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnContinue;
+    }
+}
+
---
+++ Chapter-4 -- Iteration Structures -- Loops/Ch4_Loops -- Tutorials/Ch4_Loops/VerifyPassword.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ch4_Loops
+{
+    public partial class VerifyPassword : Form
+    {
+        public VerifyPassword()
+        {
+            InitializeComponent();
+        }
+
+        private void btnContinue_Click(object sender, EventArgs e)
+        {
+            string password = null;
+            int count = 0;
+
+            while(password != "easy")
+            {
+                count = count + 1;
+                if (count > 3)
+                {
+                    MessageBox.Show("Incorrect Password. Only 3 atempts allowed.");
+                    Environment.Exit(0);
+                }
+                Console.WriteLine("Please enter password.");
+                password = Console.ReadLine();
+            }
+            MessageBox.Show(count + "Attempts. You may proceed now");
+
+        }
+    }
+}
