afb5d0ad819114dc9230509d3aee1a1dbf2338c9
vamsi-kanakm <vamsikanakam2002@gmail.com>
2023-09-16 20:05:48 -0400

    Homework Assignment Added

---
+++ Chapter-4 -- Iteration Structures -- Loops/Exercise_Ch4 -- Homework/Exercise_Ch4/LastStopBoutique.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Exercise_Ch4
+{
+    partial class LastStopBoutique
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtProductName = new System.Windows.Forms.TextBox();
+            this.txtOriginalPrice = new System.Windows.Forms.TextBox();
+            this.btnPredictCost = new System.Windows.Forms.Button();
+            this.lstOutput = new System.Windows.Forms.ListBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Product Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(147, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Original Price:";
+            // 
+            // txtProductName
+            // 
+            this.txtProductName.Location = new System.Drawing.Point(241, 69);
+            this.txtProductName.Name = "txtProductName";
+            this.txtProductName.Size = new System.Drawing.Size(100, 31);
+            this.txtProductName.TabIndex = 2;
+            // 
+            // txtOriginalPrice
+            // 
+            this.txtOriginalPrice.Location = new System.Drawing.Point(241, 128);
+            this.txtOriginalPrice.Name = "txtOriginalPrice";
+            this.txtOriginalPrice.Size = new System.Drawing.Size(100, 31);
+            this.txtOriginalPrice.TabIndex = 3;
+            // 
+            // btnPredictCost
+            // 
+            this.btnPredictCost.AutoSize = true;
+            this.btnPredictCost.Location = new System.Drawing.Point(54, 205);
+            this.btnPredictCost.Name = "btnPredictCost";
+            this.btnPredictCost.Size = new System.Drawing.Size(139, 35);
+            this.btnPredictCost.TabIndex = 4;
+            this.btnPredictCost.Text = "Predict Cost";
+            this.btnPredictCost.UseVisualStyleBackColor = true;
+            this.btnPredictCost.Click += new System.EventHandler(this.btnPredictCost_Click);
+            // 
+            // lstOutput
+            // 
+            this.lstOutput.FormattingEnabled = true;
+            this.lstOutput.ItemHeight = 25;
+            this.lstOutput.Location = new System.Drawing.Point(473, 69);
+            this.lstOutput.Name = "lstOutput";
+            this.lstOutput.Size = new System.Drawing.Size(459, 254);
+            this.lstOutput.TabIndex = 5;
+            // 
+            // btnClear
+            // 
+            this.btnClear.AutoSize = true;
+            this.btnClear.Location = new System.Drawing.Point(255, 205);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(139, 35);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.AutoSize = true;
+            this.btnExit.Location = new System.Drawing.Point(147, 288);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(139, 35);
+            this.btnExit.TabIndex = 7;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // LastStopBoutique
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1012, 450);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lstOutput);
+            this.Controls.Add(this.btnPredictCost);
+            this.Controls.Add(this.txtOriginalPrice);
+            this.Controls.Add(this.txtProductName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "LastStopBoutique";
+            this.Text = "Last Stop Boutique";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtProductName;
+        private System.Windows.Forms.TextBox txtOriginalPrice;
+        private System.Windows.Forms.Button btnPredictCost;
+        private System.Windows.Forms.ListBox lstOutput;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Chapter-4 -- Iteration Structures -- Loops/Exercise_Ch4 -- Homework/Exercise_Ch4/LastStopBoutique.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise_Ch4
+{
+    public partial class LastStopBoutique : Form
+    {
+        public LastStopBoutique()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPredictCost_Click(object sender, EventArgs e)
+        {
+            //Taking user input
+            string productName=txtProductName.Text;
+            float cost,discount;
+            if(float.TryParse(txtOriginalPrice.Text,out cost) == false)
+            {
+                MessageBox.Show("Please Enter Valid Price Value.");
+                txtOriginalPrice.Focus();
+                txtOriginalPrice.SelectAll();
+                return;
+            }
+
+            //Error Checking
+            if (cost <= 0)
+            {
+                MessageBox.Show("Invalid Price!!");
+                return;
+            }
+
+            string formatCode = "{0,7}  {1,20}";
+            lstOutput.Items.Add("The cost of the " + productName + " on: ");
+            lstOutput.Items.Add("");
+            lstOutput.Items.Add(string.Format(formatCode, "Day", "Price"));
+            lstOutput.Items.Add("-----------------------------------------------------------------------------");
+
+            //operation
+            int count =1;
+            string day = "";
+            
+
+
+            while (count <= 5)
+            {
+                switch(count)
+                {
+                    case 1:
+                        day = "Monday:";
+                        break;
+                    case 2:
+                        day = "Tuesday:";
+                        break;
+                    case 3:
+                        day = "Wednesday:";
+                        break;
+                    case 4:
+                        day = "Thursday:";
+                        break;
+                    case 5:
+                        day = "Friday:";
+                        break;
+                    default:
+                        break;
+                }
+
+                discount = cost * 0.1f;
+                cost = cost - discount;
+                count = count + 1;
+
+                lstOutput.Items.Add(string.Format("{0,7} {1,20:c}", day, cost));
+            }
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtOriginalPrice.Clear();
+            txtProductName.Clear();
+            lstOutput.Items.Clear();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
