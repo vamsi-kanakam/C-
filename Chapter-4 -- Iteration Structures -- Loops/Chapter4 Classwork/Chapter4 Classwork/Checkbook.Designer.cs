ddc708e7ccb19b5a8227436c62e0e0e75b22428c
vamsi-kanakm <vamsikanakam2002@gmail.com>
2023-09-21 11:47:43 -0400

    Chapter-4 Class Assignment added

---
+++ Chapter-4 -- Iteration Structures -- Loops/Chapter4 Classwork/Chapter4 Classwork/Checkbook.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Chapter4_Classwork
+{
+    partial class frmCheckbook
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBalance = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnBalance
+            // 
+            this.btnBalance.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBalance.Location = new System.Drawing.Point(34, 53);
+            this.btnBalance.Name = "btnBalance";
+            this.btnBalance.Size = new System.Drawing.Size(137, 63);
+            this.btnBalance.TabIndex = 0;
+            this.btnBalance.Text = "Balance Checkbook";
+            this.btnBalance.UseVisualStyleBackColor = true;
+            this.btnBalance.Click += new System.EventHandler(this.btnBalance_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutput.Location = new System.Drawing.Point(209, 53);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(179, 116);
+            this.lblOutput.TabIndex = 1;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(34, 133);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(137, 63);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmCheckbook
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(456, 264);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.btnBalance);
+            this.Name = "frmCheckbook";
+            this.Text = "Checkbook";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBalance;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Chapter-4 -- Iteration Structures -- Loops/Chapter4 Classwork/Chapter4 Classwork/Checkbook.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter4_Classwork
+{
+    public partial class frmCheckbook : Form
+    {
+        public frmCheckbook()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBalance_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Declaration
+                decimal balance = 0m;
+                string transactionType = "";
+
+                //user input
+                Console.WriteLine("Enter your Bank Balance: ");
+                balance = decimal.Parse(Console.ReadLine());
+
+                //Operatiton
+
+                Console.WriteLine("Choose your tansaction Type: ");
+                Console.WriteLine("Press 'C' -- for Checks");
+                Console.WriteLine("Press 'D' -- for Deposite");
+                Console.WriteLine("Press 'F' -- for Fee");
+                Console.WriteLine("Press 'S' -- for Stopping the Transaction");
+
+                transactionType = (Console.ReadLine()).ToUpper();
+
+                /*while (transactionType != "S")
+                {
+
+                    Console.WriteLine("Enter your transaction amount: ");
+                    decimal amount = decimal.Parse(Console.ReadLine());
+                    
+                    if (transactionType == "C" || transactionType == "F")
+                    {
+                        balance -= amount;
+                    }
+                    else if (transactionType == "D")
+                    {
+                        balance += amount;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Choose a Valid Transaction Type.");
+                    }
+
+                    Console.WriteLine("Choose your tansaction Type: ");
+                    Console.WriteLine("Press 'C' -- for Checks");
+                    Console.WriteLine("Press 'D' -- for Deposite");
+                    Console.WriteLine("Press 'F' -- for Fee");
+                    Console.WriteLine("Press 'S' -- for Stopping the Transaction");
+
+                    transactionType = (Console.ReadLine()).ToUpper();
+                }*/
+
+                do
+                {
+                    Console.WriteLine("Choose your tansaction Type: ");
+                    Console.WriteLine("Press 'C' -- for Checks");
+                    Console.WriteLine("Press 'D' -- for Deposite");
+                    Console.WriteLine("Press 'F' -- for Fee");
+                    Console.WriteLine("Press 'S' -- for Stopping the Transaction");
+
+                    transactionType = (Console.ReadLine()).ToUpper();
+                    decimal amount = 0;
+
+                    if (transactionType != "S")
+                    {
+                        Console.WriteLine("Enter your transaction amount: ");
+                        amount = decimal.Parse(Console.ReadLine());
+                    }
+
+                    if (transactionType == "C" || transactionType == "F")
+                    {
+                        balance -= amount;
+                    }
+                    else if (transactionType == "D")
+                    {
+                        balance += amount;
+                    }
+                    else if (transactionType == "S")
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Choose a Valid Transaction Type.");
+                    }
+                }
+                while (transactionType != "S");
+
+                //Output
+                lblOutput.Text = "The final balance : " + balance.ToString("C");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            //exit the application
+            this.Close();
+        }
+    }
+}
