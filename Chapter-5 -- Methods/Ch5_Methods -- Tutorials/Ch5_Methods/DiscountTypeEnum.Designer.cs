f2da4cb8b7f3a6f58e1eb60fd746b51e7ad46860
vamsi-kanakm <vamsikanakam2002@gmail.com>
2023-09-28 15:45:18 -0400

    Added

---
+++ Chapter-5 -- Methods/Ch5_Methods -- Tutorials/Ch5_Methods/DiscountTypeEnum.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Ch5_Methods
+{
+    partial class DiscountTypeEnum
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboDiscountType = new System.Windows.Forms.ComboBox();
+            this.btnComputeRate = new System.Windows.Forms.Button();
+            this.lstOutput = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // cboDiscountType
+            // 
+            this.cboDiscountType.FormattingEnabled = true;
+            this.cboDiscountType.Location = new System.Drawing.Point(56, 109);
+            this.cboDiscountType.Name = "cboDiscountType";
+            this.cboDiscountType.Size = new System.Drawing.Size(175, 33);
+            this.cboDiscountType.TabIndex = 0;
+            this.cboDiscountType.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // btnComputeRate
+            // 
+            this.btnComputeRate.AutoSize = true;
+            this.btnComputeRate.Location = new System.Drawing.Point(311, 109);
+            this.btnComputeRate.Name = "btnComputeRate";
+            this.btnComputeRate.Size = new System.Drawing.Size(159, 35);
+            this.btnComputeRate.TabIndex = 1;
+            this.btnComputeRate.Text = "Compute Rate";
+            this.btnComputeRate.UseVisualStyleBackColor = true;
+            this.btnComputeRate.Click += new System.EventHandler(this.btnComputeRate_Click);
+            // 
+            // lstOutput
+            // 
+            this.lstOutput.FormattingEnabled = true;
+            this.lstOutput.ItemHeight = 25;
+            this.lstOutput.Location = new System.Drawing.Point(311, 164);
+            this.lstOutput.Name = "lstOutput";
+            this.lstOutput.Size = new System.Drawing.Size(384, 79);
+            this.lstOutput.TabIndex = 2;
+            // 
+            // DiscountTypeEnum
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 309);
+            this.Controls.Add(this.lstOutput);
+            this.Controls.Add(this.btnComputeRate);
+            this.Controls.Add(this.cboDiscountType);
+            this.Name = "DiscountTypeEnum";
+            this.Text = "DiscountTypeEnum";
+            this.Load += new System.EventHandler(this.DiscountTypeEnum_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboDiscountType;
+        private System.Windows.Forms.Button btnComputeRate;
+        private System.Windows.Forms.ListBox lstOutput;
+    }
+}
---
+++ Chapter-5 -- Methods/Ch5_Methods -- Tutorials/Ch5_Methods/DiscountTypeEnum.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ch5_Methods
+{
+    public partial class DiscountTypeEnum : Form
+    {
+        public DiscountTypeEnum()
+        {
+            InitializeComponent();
+        }
+
+        enum discountTypes { NoDiscount,AAA,Govt,Senior};
+        discountTypes selectedDiscType;
+
+        private void DiscountTypeEnum_Load(object sender, EventArgs e)
+        {
+            string[] discTypes = {"No Discount","AAA","Federal Government","Senior"};
+            for(int i = 0; i <= 3; i++)
+            {
+                cboDiscountType.Items.Add(discTypes[i]);
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch(cboDiscountType.SelectedItem.ToString())
+            {
+                case "No Discount":
+                    selectedDiscType = discountTypes.NoDiscount; 
+                    break;
+                case "AAA":
+                    selectedDiscType = discountTypes.AAA; 
+                    break;
+                case "Federal Government":
+                    selectedDiscType = discountTypes.Govt;
+                    break;
+                case "Senior":
+                    selectedDiscType = discountTypes.Senior;
+                    break;
+            }
+        }
+
+        private float RoomRate(discountTypes discountType)
+        {
+            float rate = 0;
+            switch(discountType)
+            {
+                case discountTypes.NoDiscount:
+                    rate = 125;
+                    break;
+                case discountTypes.AAA:
+                    rate = 110;
+                    break;
+                case discountTypes.Govt:
+                    rate = 120;
+                    break;
+                case discountTypes.Senior:
+                    rate = 105;
+                    break;
+            }
+            return rate;
+        }
+
+        private void btnComputeRate_Click(object sender, EventArgs e)
+        {
+            float roomRate = RoomRate(selectedDiscType);
+
+            lstOutput.Items.Clear();
+            lstOutput.Items.Add("Rate for disc. type, " + selectedDiscType + ": " + roomRate.ToString("C"));
+        }
+    }
+}
