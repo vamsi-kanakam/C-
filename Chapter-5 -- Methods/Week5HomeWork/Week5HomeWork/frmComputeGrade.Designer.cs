feda3c7960dc545ae370d1724ec6ea5957daff2d
vamsi-kanakm <vamsikanakam2002@gmail.com>
2023-10-03 19:37:28 -0400

    Homework added.

---
+++ Chapter-5 -- Methods/Week5HomeWork/Week5HomeWork/frmComputeGrade.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Week5HomeWork
+{
+    partial class frmComputeGrade
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtScore = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnComputeGrade = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lstOutput = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Score: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Subject Name:";
+            // 
+            // txtScore
+            // 
+            this.txtScore.Location = new System.Drawing.Point(226, 88);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(155, 31);
+            this.txtScore.TabIndex = 2;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(226, 154);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(155, 31);
+            this.txtName.TabIndex = 3;
+            // 
+            // btnComputeGrade
+            // 
+            this.btnComputeGrade.AutoSize = true;
+            this.btnComputeGrade.Location = new System.Drawing.Point(112, 245);
+            this.btnComputeGrade.Name = "btnComputeGrade";
+            this.btnComputeGrade.Size = new System.Drawing.Size(173, 35);
+            this.btnComputeGrade.TabIndex = 4;
+            this.btnComputeGrade.Text = "Compute Grade";
+            this.btnComputeGrade.UseVisualStyleBackColor = true;
+            this.btnComputeGrade.Click += new System.EventHandler(this.btnComputeGrade_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.AutoSize = true;
+            this.btnClear.Location = new System.Drawing.Point(37, 323);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(112, 35);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.AutoSize = true;
+            this.btnExit.Location = new System.Drawing.Point(242, 323);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(112, 35);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lstOutput
+            // 
+            this.lstOutput.FormattingEnabled = true;
+            this.lstOutput.ItemHeight = 25;
+            this.lstOutput.Location = new System.Drawing.Point(460, 245);
+            this.lstOutput.Name = "lstOutput";
+            this.lstOutput.Size = new System.Drawing.Size(416, 104);
+            this.lstOutput.TabIndex = 7;
+            // 
+            // frmComputeGrade
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(914, 450);
+            this.Controls.Add(this.lstOutput);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnComputeGrade);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmComputeGrade";
+            this.Text = "frmCompute Grade";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtScore;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnComputeGrade;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ListBox lstOutput;
+    }
+}
+
---
+++ Chapter-5 -- Methods/Week5HomeWork/Week5HomeWork/frmComputeGrade.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week5HomeWork
+{
+    public partial class frmComputeGrade : Form
+    {
+        public frmComputeGrade()
+        {
+            InitializeComponent();
+        }
+
+        bool flag;
+        private void btnComputeGrade_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string name;
+                float score;
+                flag = false;
+
+                //call Validscore
+                (name, score) = ValidScore();
+
+                //if invalid score terminate program
+                if (flag)
+                {
+                    return;
+                }
+                //call CalculateGrate
+                string grade = CalculateGrade(score);
+                //call DisplayResult
+                DisplayResult(name, grade);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private (string , float) ValidScore()
+        {
+            //user input
+            string name = txtName.Text;
+            float score;
+            
+            if((float.TryParse(txtScore.Text,out score)) == false)
+            {
+                MessageBox.Show("Invalid Score!!");
+                txtScore.Focus();
+                txtScore.SelectAll();
+            }
+            //score validation
+            if (score < 0 || score > 100)
+            {
+                MessageBox.Show("Invalid Score!!");
+                txtScore.Focus();
+                txtScore.SelectAll();
+                flag = true;
+            }
+
+            return (name, score);
+        }
+
+        private string CalculateGrade(float score)
+        {
+            string grade=null;
+            if(score>=90 && score <= 100)
+            {
+                grade = "A";
+            }
+            else if(score>=80 && score < 90)
+            {
+                grade = "B";
+            }
+            else if(score>=70 && score < 80)
+            {
+                grade = "C";
+            }
+            else if(score>=60 && score < 70)
+            {
+                grade = "D";
+            }
+            else
+            {
+                grade = "F";
+            }
+            return grade;
+        }
+
+        private void DisplayResult(string name,string grade)
+        {
+            lstOutput.Items.Add(string.Format("{0,20}{1,10}","Subject: ",name));
+            lstOutput.Items.Add("");
+            lstOutput.Items.Add(string.Format("{0,20}{1,10}", "Grade: ", grade));
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lstOutput.Items.Clear();
+            txtName.Clear();
+            txtScore.Clear();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
